d048509f5714454c3f04350369e7239bdc211ff4
md2060100-commits <md2060100@gmail.com>
2026-06-01 15:19:14 +0600

    Update emojiVoice.js

--- Script/commands/emojiVoice.js
+++ Script/commands/emojiVoice.js
@@ -44,7 +44,7 @@ const emojiAudioMap = {
  "🥹": "https://files.catbox.moe/jf85xe.mp3",
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
- "🐸": "https://files.catbox.moe/utl83s.mp3"
+ "🎸": "https://files.catbox.moe/cx1in4.mp3",
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
